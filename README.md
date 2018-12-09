29a878a17d8b38f296d9a5e23143e0c7386e0511
Edward Paget <edward@cassetteta.pe>
2018-12-09 14:28:29 -0500

    Add compose configuration to start all containers
    Clean up promise logic in monitor

---
+++ README.md
@@ -0,0 +1,5 @@
+# SAML/OAuth Proxies
+
+```
+docker-compose -f saml-proxy/docker-compose.yml -f oauth-proxy/docker-compose.yml -f proxy-monitor/docker-compose.yml
+```
--- proxy-monitor/index.js
+++ proxy-monitor/index.js
@@ -9,29 +9,27 @@ const { saml_proxy_endpoint, oauth_proxy_endpoint, port } = config;
 
 function ping(uri) {
   return request({method: 'GET', uri: uri, resolveWithFullResponse: true})
-      .then( (response) => {
+      .then( response => {
         console.log(uri, response.statusCode)
         return 'ok';
       })
-      .catch( (err) => {
-        console.log(uri, err.message)
+      .catch( err => {
+        console.error(uri, err.message)
         return err.message;
       });
-} 
+}
 
 function startApp() {
   const app = express();
 
   app.get('/health-check', async (req, res) => {
-    const samlPromise = ping(saml_proxy_endpoint);
-    const oauthPromise = ping(oauth_proxy_endpoint);
-    const samlStatus = await samlPromise;
-    const oauthStatus = await oauthPromise;
+    const saml = await ping(saml_proxy_endpoint);
+    const oauth = await ping(oauth_proxy_endpoint);
 
     if (samlStatus === 'ok' && oauthStatus == 'ok') {
-      res.status(200).send(JSON.stringify({'saml': samlStatus, 'oauth': oauthStatus}))
+      res.status(200).json({saml, oauth});
     } else {
-      res.status(500).send(JSON.stringify({'saml': samlStatus, 'oauth': oauthStatus}))
+      res.status(500).json({saml, oauth});
     }
   });
 
@@ -39,14 +37,12 @@ function startApp() {
   return app;
 }
 
-(async () => {
-  try {
-    startApp();
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-})();
+try {
+  startApp();
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
 
 module.exports = {
   startApp,
