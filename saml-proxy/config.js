82466832ac72ce02fe6a6c9148865d5087c6b45d
Drew Vogel <drew@adhocteam.us>
2019-01-23 17:37:02 -0600

    Empty out idpConfig file.

---
+++ saml-proxy/config.js
@@ -0,0 +1,6 @@
+// This file used to be used to configure the IDP attribute metadata. That was
+// removed but this configuration shell is being retained because it is likely
+// that we will need to introduce IDP configuration parameters again in the
+// future.
+
+module.exports = {}
