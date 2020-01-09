4bdd1584465a50829a6c36e1c52d4af215c6b036
Drew Vogel <drew@adhocteam.us>
2020-01-08 19:44:19 -0500

    Extract larger route handlers into their own files.

---
+++ oauth-proxy/oauthHandlers/index.js
@@ -0,0 +1,7 @@
+// This module should eventually contain an express route handler for each
+// official OAuth 2.0 endpoint.
+module.exports = {
+  'authorizeHandler': require('./authorizeHandler'),
+  'tokenHandler': require('./tokenHandler'),
+  'redirectHandler': require('./redirectHandler'),
+};
