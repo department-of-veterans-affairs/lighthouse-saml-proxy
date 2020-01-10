dbd306f7d7d31ca138571629c94b7f805565a789
Josh Salisbury <josh@adhocteam.us>
2020-01-10 11:25:03 -0600

    Clarify functional tests
    Added an example of the SAMLReponse we are parsing. Also pointed to the
    html documents being parsed. Hopefully rearranging helps clarify the
    functional tests a bit

--- saml-proxy/test/e2e.test.js
+++ saml-proxy/test/e2e.test.js
@@ -10,7 +10,7 @@ import { getApp, idpConfig } from './testServer';
 const { startServerInBackground, stopBackgroundServer } = buildBackgroundServerModule("saml-proxy test app");
 
 const loaRedirect = 'Found. Redirecting to /samlproxy/sp/verify?authnContext=http%3A%2F%2Fidmanagement.gov%2Fns%2Fassurance%2Floa%2F3';
-const userNotFoundTitle = 'Choose Login Provider';
+const userNotFoundText = 'We need to verify your identity before giving you access to your information';
 
 const ERROR = 'error';
 const LOA_REDIRECT = 'loa_redirect';
@@ -50,6 +50,23 @@ function buildSamlResponse(firstname, level_of_assurance) {
   });
 }
 
+function ssoRequest(samlResponse, state = 'state') {
+  const reqOpts = {
+    method: 'POST',
+    resolveWithFullResponse: true,
+    simple: false,
+    uri: `http://localhost:${PORT}/samlproxy/sp/saml/sso`,
+    form: {
+      SAMLResponse: samlResponse,
+      RelayState: state,
+    }
+  };
+
+  return request(reqOpts);
+}
+
+// These are the HTML parsers. See https://github.com/auth0/node-samlp/blob/master/templates/form.ejs
+// for the html document being parsed
 function stateFromHtml(html) {
   const parser = new DOMParser();
   const parsed = parser.parseFromString(html, MIME_HTML);
@@ -64,6 +81,19 @@ function SAMLResponseFromHtml(html) {
   return elementValue(inputs, 'SAMLResponse');
 }
 
+// This function looks for the userNotFoundText in src/views/icnError.hbs
+function isUserNotFound(body) {
+  const parser = new DOMParser();
+  const parsed = parser.parseFromString(body, MIME_HTML);
+  const h3s = parsed.getElementsByTagName('h3');
+  for(const h3 of h3s) {
+    if(h3.textContent.trim() === userNotFoundText) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function elementValue(elements, name) {
   for(const element of elements) {
     if(element.getAttributeNode('name').nodeValue === name) {
@@ -72,6 +102,8 @@ function elementValue(elements, name) {
   }
 }
 
+// These are the SAMLResponse parsers. See SAMLResponse.example.xml in the current dir (test)
+// for an example of the xml document we are parsing
 function assertionValueFromSAMLResponse(samlResponse, assertion) {
   const element = findAssertionInSamlResponse(samlResponse, assertion);
   if(!element) {
@@ -83,13 +115,25 @@ function assertionValueFromSAMLResponse(samlResponse, assertion) {
 function findAssertionInSamlResponse(samlResponse, assertion) {
   const parser = new DOMParser();
   const parsed = parser.parseFromString(samlResponse);
-  const attributes = parsed.getElementsByTagName('saml:Attribute');
-  for(const element of attributes) {
-    for(const inner of element.attributes) {
-      if(inner.nodeValue === assertion) {
+  const elements = parsed.getElementsByTagName('saml:Attribute');
+  for(const element of elements) {
+    for(const attribute of element.attributes) {
+      if(attribute.nodeValue === assertion) {
         return element;
       }
     }
+  }
+}
+
+function isBodySamlResponse(body) {
+  const parser = new DOMParser();
+  try {
+    const samlResponse = atob(SAMLResponseFromHtml(body));
+    const parsed = parser.parseFromString(samlResponse);
+    const found = parsed.getElementsByTagName('samlp:Response');
+    return found.length > 0;
+  } catch {
+    return false
   }
 }
 
@@ -105,7 +149,7 @@ function responseResultType(response) {
     return LOA_REDIRECT;
   }
 
-  if(pageTitle(body) === userNotFoundTitle) {
+  if(isUserNotFound(body)) {
     return USER_NOT_FOUND;
   }
 
@@ -114,39 +158,6 @@ function responseResultType(response) {
   }
 
   return UNKNOWN;
-}
-
-function isBodySamlResponse(body) {
-  const parser = new DOMParser();
-  const samlResponse = atob(SAMLResponseFromHtml(body));
-  const parsed = parser.parseFromString(samlResponse);
-  const found = parsed.getElementsByTagName('samlp:Response');
-  return found.length > 0;
-}
-
-function pageTitle(body) {
-  const parser = new DOMParser();
-  const parsed = parser.parseFromString(body, MIME_HTML);
-  const titles = parsed.getElementsByTagName('title');
-  const title = titles[0];
-  if(title) {
-    return title.textContent;
-  }
-}
-
-function ssoRequest(samlResponse, state = 'state') {
-  const reqOpts = {
-    method: 'POST',
-    resolveWithFullResponse: true,
-    simple: false,
-    uri: `http://localhost:${PORT}/samlproxy/sp/saml/sso`,
-    form: {
-      SAMLResponse: samlResponse,
-      RelayState: state,
-    }
-  };
-
-  return request(reqOpts);
 }
 
 describe('Logins for idp', () => {
