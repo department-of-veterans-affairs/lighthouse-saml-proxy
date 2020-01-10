db8fd6c7e33917747397c09672aebd29877b523f
Drew Vogel <drew@adhocteam.us>
2020-01-10 10:56:18 -0500

    Refactor token validation to enable e2e test coverage for launch/patient scope. (#79)

--- oauth-proxy/utils.js
+++ oauth-proxy/utils.js
@@ -5,6 +5,24 @@ function statusCodeFromError(error) {
   return 500;
 }
 
+const isRuntimeError = (err) => {
+  return (
+    (err instanceof EvalError)
+    || (err instanceof ReferenceError)
+    || (err instanceof RangeError)
+    || (err instanceof SyntaxError)
+    || (err instanceof TypeError)
+  );
+};
+
+const rethrowIfRuntimeError = (err) => {
+  if (isRuntimeError(err)) {
+    throw err;
+  }
+};
+
 module.exports = {
+  isRuntimeError,
+  rethrowIfRuntimeError,
   statusCodeFromError,
 }
