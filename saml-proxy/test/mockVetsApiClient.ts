6808a1f669d1d26328f952a5b38e61c442d1d9d9
Beau Grantham <beau.grantham@va.gov>
2020-09-09 15:59:26 -0400

    Remove unused functions

--- saml-proxy/test/mockVetsApiClient.ts
+++ saml-proxy/test/mockVetsApiClient.ts
@@ -1,35 +1,6 @@
-import { SAMLUser } from "../src/VetsAPIClient";
-
 export default class MockVetsApiClient {
   public findUserInMVI = true;
   public userIsVSO = true;
-
-  public async getMVITraitsForLoa3User(
-    user: SAMLUser
-  ): Promise<{ icn: string; first_name: string; last_name: string }> {
-    if (this.findUserInMVI) {
-      return {
-        icn: "123",
-        first_name: user.firstName,
-        last_name: user.lastName,
-      };
-    }
-
-    throw new Error("Not found");
-  }
-
-  public async getVSOSearch(
-    firstName: string, // eslint-disable-line
-    lastName: string // eslint-disable-line
-  ): Promise<{ poa: string }> {
-    if (this.userIsVSO) {
-      return {
-        poa: "poa",
-      };
-    }
-
-    throw new Error("Not found");
-  }
 
   public reset() {
     this.findUserInMVI = true;
