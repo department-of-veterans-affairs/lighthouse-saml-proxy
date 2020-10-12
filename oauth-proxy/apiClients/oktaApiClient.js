7b612ec010958d9c7f19b0d59b48960f573d271a
Larry Crochet <larry.crochet@libertyits.com>
2020-10-12 19:22:56 -0400

    lint items

--- oauth-proxy/apiClients/oktaApiClient.js
+++ oauth-proxy/apiClients/oktaApiClient.js
@@ -1,6 +1,11 @@
 const uriTemplates = require("uri-templates");
 
-const deleteUserGrantOnClient = async (oktaClient, config, userId, clientId) => {
+const deleteUserGrantOnClient = async (
+  oktaClient,
+  config,
+  userId,
+  clientId
+) => {
   let error;
   let response;
   const template = uriTemplates(
@@ -14,7 +19,6 @@ const deleteUserGrantOnClient = async (oktaClient, config, userId, clientId) => 
   if (response === undefined) {
     throw error;
   }
-  
   return response;
 };
 
@@ -23,13 +27,15 @@ const getUserIds = async (okta_client, email) => {
   let userIds = [];
 
   await okta_client.listUsers({
-    search: emailFilter
+    search: emailFilter,
   })
-  .each(user => userIds.push(user.id))
-  .catch(err => {throw err});
+    .each((user) => userIds.push(user.id))
+    .catch((err) => {
+      throw err;
+    });
 
   if (!userIds.length) {
-    throw {status: 400, errorMessage: "Invalid email"};
+    throw { status: 400, errorMessage: "Invalid email" };
   }
 
   return userIds;
@@ -40,25 +46,37 @@ const getClientInfo = async (oktaClient, config, clientId) => {
     config.okta_url + "/oauth2/v1/clients/{clientid}"
   );
 
-  let response = await callOktaEndpoint(oktaClient, template.fill({ clientid: clientId }));
+  let response = await callOktaEndpoint(
+    oktaClient,
+    template.fill({ clientid: clientId })
+  );
   return response;
 };
 
-const getAuthorizationServerInfo = async (config, authorizationServerId, oktaClient) => {
+const getAuthorizationServerInfo = async (
+  config,
+  authorizationServerId,
+  oktaClient
+) => {
   const template = uriTemplates(
     config.okta_url + "/api/v1/authorizationServers/{authorizationServerId}"
   );
 
-  let response = await callOktaEndpoint(oktaClient, template.fill({ authorizationServerId: authorizationServerId }));
+  let response = await callOktaEndpoint(
+    oktaClient,
+    template.fill({ authorizationServerId: authorizationServerId })
+  );
   return response;
 };
 
 const getClaims = async (authorizationServerId, oktaClient) => {
   let claims = [];
-  const claimsCollection = await oktaClient.listOAuth2Claims(authorizationServerId);
-  await claimsCollection.each(claim => {
+  const claimsCollection = await oktaClient.listOAuth2Claims(
+    authorizationServerId
+  );
+  await claimsCollection.each((claim) => {
     claims.push(claim.name);
-  })
+  });
   return claims;
 };
 
@@ -66,9 +84,10 @@ const getClaims = async (authorizationServerId, oktaClient) => {
 async function callOktaEndpoint(oktaClient, oktaUrl, method) {
   let error;
   let response;
-  method = method === undefined ? 'get' : method;
-  await oktaClient.http.http(oktaUrl, { method: method })
-    .then((res) => (res.text()))
+  method = method === undefined ? "get" : method;
+  await oktaClient.http
+    .http(oktaUrl, { method: method })
+    .then((res) => res.text())
     .then((text) => (response = JSON.parse(text)))
     .catch((err) => (error = err));
 
@@ -76,14 +95,12 @@ async function callOktaEndpoint(oktaClient, oktaUrl, method) {
     throw error;
   }
   return response;
-};
+}
 
 module.exports = {
   deleteUserGrantOnClient,
   getUserIds,
   getClientInfo,
   getAuthorizationServerInfo,
-  getClaims
+  getClaims,
 };
-
-
