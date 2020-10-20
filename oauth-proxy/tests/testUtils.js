3d0ec83129db4e0310075e33d8885176a7df63cd
Luke Markwordt <luke.markwordt@libertyits.com>
2020-10-20 13:58:32 -0600

    Moved all test helper methods from oauthHandlers.test.js to testUtils.js

--- oauth-proxy/tests/testUtils.js
+++ oauth-proxy/tests/testUtils.js
@@ -1,3 +1,5 @@
+const { RequestError } = require("request-promise-native/errors");
+
 function buildDynamoAttributeValue(value) {
   // BEWARE: This doesn't work with number sets and a few other Dynamo types.
   if (value.constructor === String) {
@@ -26,7 +28,199 @@ function convertObjectToDynamoAttributeValues(obj) {
   }, {});
 }
 
+function buildFakeDynamoClient(fakeDynamoRecord) {
+  const dynamoClient = jest.genMockFromModule("../dynamo_client.js");
+  dynamoClient.saveToDynamo.mockImplementation((state) => {
+    return new Promise((resolve) => {
+      // It's unclear whether this should resolve with a full records or just
+      // the identity field but thus far it has been irrelevant to the
+      // functional testing of the oauth-proxy.
+      resolve({ pk: state });
+    });
+  });
+  dynamoClient.getFromDynamoBySecondary.mockImplementation(
+    (handle, attr, value) => {
+      return new Promise((resolve, reject) => {
+        if (fakeDynamoRecord[attr] === value) {
+          resolve(convertObjectToDynamoAttributeValues(fakeDynamoRecord));
+        } else {
+          reject(`no such ${attr} value`);
+        }
+      });
+    }
+  );
+  dynamoClient.getFromDynamoByState.mockImplementation((handle, state) => {
+    return new Promise((resolve, reject) => {
+      if (state === fakeDynamoRecord.state) {
+        resolve(convertObjectToDynamoAttributeValues(fakeDynamoRecord));
+      } else {
+        reject("no such state value");
+      }
+    });
+  });
+  return dynamoClient;
+}
+
+class FakeIssuer {
+  constructor(client) {
+    this.Client = class FakeInlineClient {
+      constructor() {
+        return client;
+      }
+    };
+    this.metadata = {
+      issuer: "https://fake.okta.com/oauth2/1234",
+      authorization_endpoint: "fake_enpoint",
+    };
+  }
+}
+
+function buildFakeOktaClient(
+  fakeRecord,
+  getAuthorizationServerInfoMock,
+  userCollection
+) {
+  const oClient = { getApplication: jest.fn(), listUsers: jest.fn() };
+  oClient.getApplication.mockImplementation((client_id) => {
+    return new Promise((resolve, reject) => {
+      if (client_id === fakeRecord.client_id) {
+        resolve(fakeRecord);
+      } else {
+        reject(`no such client application '${client_id}'`);
+      }
+    });
+  });
+  oClient.getAuthorizationServer = getAuthorizationServerInfoMock;
+  oClient.listUsers = () => {
+    return userCollection;
+  };
+  return oClient;
+}
+
+const buildFakeGetAuthorizationServerInfoResponse = (audiences) => {
+  return {
+    id: "id",
+    name: "name",
+    description: "description",
+    audiences: audiences,
+    issuer: "https://fake.okta.com/oauth2/1234",
+    issuerMode: "ORG_URL",
+    status: "ACTIVE",
+    created: "2000-01-01T00:00:00.000Z",
+    lastUpdated: "2000-01-01T00:00:00.000Z",
+    credentials: {
+      signing: {
+        rotationMode: "AUTO",
+        lastRotated: "2000-01-01T00:00:00.000Z",
+        nextRotation: "2000-01-01T00:00:00.000Z",
+        kid: "kid",
+      },
+    },
+    _links: {
+      rotateKey: {
+        href: "https://fake.okta.com/oauth2/1234/keyRotate",
+        hints: {
+          allow: ["POST"],
+        },
+      },
+      metadata: [
+        {
+          name: "oauth-authorization-server",
+          href:
+            "https://fake.okta.com/oauth2/1234/.well-known/oauth-authorization-server",
+          hints: {
+            allow: ["GET"],
+          },
+        },
+        {
+          name: "openid-configuration",
+          href:
+            "https://fake.okta.com/oauth2/1234/.well-known/openid-configuration",
+          hints: {
+            allow: ["GET"],
+          },
+        },
+      ],
+      keys: {
+        href:
+          "https://fake.okta.com/oauth2/1234/api/v1/authorizationServers/default/credentials/keys",
+        hints: {
+          allow: ["GET"],
+        },
+      },
+      claims: {
+        href:
+          "https://fake.okta.com/oauth2/1234/api/v1/authorizationServers/default/claims",
+        hints: {
+          allow: ["GET", "POST"],
+        },
+      },
+      policies: {
+        href:
+          "https://fake.okta.com/oauth2/1234/api/v1/authorizationServers/default/policies",
+        hints: {
+          allow: ["GET", "POST"],
+        },
+      },
+      self: {
+        href:
+          "https://fake.okta.com/oauth2/1234/api/v1/authorizationServers/default",
+        hints: {
+          allow: ["GET", "DELETE", "PUT"],
+        },
+      },
+      scopes: {
+        href:
+          "https://fake.okta.com/oauth2/1234/api/v1/authorizationServers/default/scopes",
+        hints: {
+          allow: ["GET", "POST"],
+        },
+      },
+      deactivate: {
+        href:
+          "https://fake.okta.com/oauth2/1234/api/v1/authorizationServers/default/lifecycle/deactivate",
+        hints: {
+          allow: ["POST"],
+        },
+      },
+    },
+  };
+};
+
+const buildOpenIDClient = (fns) => {
+  let client = {};
+  for (let [fn_name, fn_impl] of Object.entries(fns)) {
+    client[fn_name] = jest.fn().mockImplementation(async () => {
+      return new Promise((resolve, reject) => {
+        fn_impl(resolve, reject);
+      });
+    });
+  }
+  return client;
+};
+
+const buildExpiredRefreshTokenClient = () => {
+  return buildOpenIDClient({
+    refresh: () => {
+      // This simulates an upstream error so that we don't have to test the full handler.
+      throw new RequestError(
+        new Error(
+          "simulated upstream response error for expired refresh token"
+        ),
+        {},
+        { statusCode: 400 }
+      );
+    },
+  });
+};
+
 module.exports = {
   buildDynamoAttributeValue,
   convertObjectToDynamoAttributeValues,
+  buildFakeOktaClient,
+  buildFakeDynamoClient,
+  buildFakeGetAuthorizationServerInfoResponse,
+  buildOpenIDClient,
+  buildExpiredRefreshTokenClient,
+  FakeIssuer,
 };
