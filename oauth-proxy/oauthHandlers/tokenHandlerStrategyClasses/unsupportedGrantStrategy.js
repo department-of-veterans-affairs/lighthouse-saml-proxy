d4f8047e85fb22c568bf04eb67afeaecfd159003
Luke Markwordt <luke.markwordt@libertyits.com>
2020-10-23 14:19:06 -0600

    added tokenHandlerError to UnsupportedGrantStrategy

--- oauth-proxy/oauthHandlers/tokenHandlerStrategyClasses/unsupportedGrantStrategy.js
+++ oauth-proxy/oauthHandlers/tokenHandlerStrategyClasses/unsupportedGrantStrategy.js
@@ -1,13 +1,13 @@
+const { TokenHandlerError } = require("./tokenHandlerErrors");
 class UnsupportedGrantStrategy {
   constructor() {}
 
   async getToken(client, redirect_uri) { // eslint-disable-line
-    throw {
-      statusCode: 400,
-      error: "unsupported_grant_type",
-      error_description:
-        "Only authorization and refresh_token grant types are supported",
-    };
+    throw new TokenHandlerError(
+      "unsupported_grant_type",
+      "Only authorization and refresh_token grant types are supported",
+      400
+    );
   }
 }
 
