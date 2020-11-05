a10fa7056cd0ff222dbd62247e1af0ebd259de39
Luke Markwordt <luke.markwordt@libertyits.com>
2020-11-05 14:45:57 -0700

    Added saveDocumentLaunchStrategy class

---
+++ oauth-proxy/oauthHandlers/tokenHandlerStrategyClasses/saveDocumentStrategies/saveDocumentLaunchStrategy.js
@@ -0,0 +1,6 @@
+class SaveDocumentLaunchStrategy {
+  // eslint-disable-next-line no-unused-vars
+  async saveDocumentToDynamo(document, tokens) {}
+}
+
+module.exports = { SaveDocumentLaunchStrategy };
