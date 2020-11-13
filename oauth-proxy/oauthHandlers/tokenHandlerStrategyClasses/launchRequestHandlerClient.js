601044dd4270dda07567297cb1f8482a3f152439
Larry Crochet <larry.crochet@libertyits.com>
2020-11-13 07:51:36 -0500

    lint

--- oauth-proxy/oauthHandlers/tokenHandlerStrategyClasses/launchRequestHandlerClient.js
+++ oauth-proxy/oauthHandlers/tokenHandlerStrategyClasses/launchRequestHandlerClient.js
@@ -1,20 +1,12 @@
 const { hashString } = require("../../utils");
 const jwtDecode = require("jwt-decode");
-const { rethrowIfRuntimeError } = require("../../utils");
-const { translateTokenSet } = require("../tokenResponse");
-const { PullDocumentByAccessTokenStrategy } = require("./pullDocumentStrategies/pullDocumentByAccessTokenStrategy");
-const createError = require('http-errors')
+const {
+  PullDocumentByAccessTokenStrategy,
+} = require("./pullDocumentStrategies/pullDocumentByAccessTokenStrategy");
+const createError = require("http-errors");
 
 class LaunchRequestHandlerClient {
-  constructor(
-    config,
-    logger,
-    dynamo,
-    dynamoClient,
-    req,
-    res,
-    next
-  ) {
+  constructor(config, logger, dynamo, dynamoClient, req, res, next) {
     this.pullDocumentFromDynamoStrategy = new PullDocumentByAccessTokenStrategy(
       logger,
       dynamo,
@@ -32,7 +24,8 @@ class LaunchRequestHandlerClient {
       error.statusCode = 401;
       throw error;
     }
-    const token_index = this.req.headers.authorization.indexOf("Bearer") + "Bearer ".length;
+    const token_index =
+      this.req.headers.authorization.indexOf("Bearer") + "Bearer ".length;
     const access_token = this.req.headers.authorization.substr(token_index);
     let documentResponse = await this.pullDocumentFromDynamoStrategy.pullDocumentFromDynamo(
       access_token
