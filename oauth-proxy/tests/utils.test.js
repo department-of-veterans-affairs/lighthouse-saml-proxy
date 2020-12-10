c01b0d8dff33ff5ed27c2b57e5b3ad3405d6af3f
Larry Crochet <larry.crochet@libertyits.com>
2020-12-10 10:13:00 -0500

    Add unit test  highligting problem and fix

--- oauth-proxy/tests/utils.test.js
+++ oauth-proxy/tests/utils.test.js
@@ -7,6 +7,7 @@ const {
   parseClientId,
   parseBearerAuthorization,
   hashString,
+  minimalError,
 } = require("../utils");
 
 describe("statusCodeFromError", () => {
@@ -166,5 +167,85 @@ describe("parseBearerAuthorization", () => {
   });
   it("Match", () => {
     expect(parseBearerAuthorization("Bearer jwt")).toBe("jwt");
+  });
+});
+
+describe("minimalError", () => {
+  it("API-3493 verbose log on timeout fix verification", () => {
+    let testError = {
+      service: "oauth-proxy",
+      name: "TimeoutError",
+      code: "ETIMEDOUT",
+      host: "deptva-eval.okta.com",
+      hostname: "deptva-eval.okta.com",
+      method: "POST",
+      path: "/oauth2/xxx/v1/token",
+      protocol: "https:",
+      url: "https://deptva-eval.okta.com/oauth2/xxxxxx/v1/token",
+      gotOptions: {
+        path: "/oauth2/aus7y0ho1w0bSNLDV2p7/v1/token",
+        protocol: "https:",
+        slashes: true,
+        auth: null,
+        host: "deptva-eval.okta.com",
+        port: null,
+        hostname: "deptva-eval.okta.com",
+        hash: null,
+        search: null,
+        query: null,
+        pathname: "/oauth2/xxx/v1/token",
+        href: "https://xxxxoauth2/aus7y0ho1w0bSNLDV2p7/v1/token",
+        retry: {
+          methods: {},
+          statusCodes: {},
+          errorCodes: {},
+          maxRetryAfter: 10000,
+        },
+        headers: {
+          "user-agent":
+            "openid-client/3.15.10 (https://github.com/panva/node-openid-client)",
+          authorization: "Basic xxxx==",
+          accept: "application/json",
+          "accept-encoding": "gzip, deflate",
+          "content-type": "application/x-www-form-urlencoded",
+          "content-length": 141,
+        },
+        hooks: {
+          beforeError: [],
+          init: [],
+          beforeRequest: [],
+          beforeRedirect: [],
+          beforeRetry: [],
+          afterResponse: [],
+        },
+        decompress: true,
+        throwHttpErrors: false,
+        followRedirect: false,
+        stream: false,
+        form: true,
+        json: true,
+        cache: false,
+        useElectronNet: false,
+        gotTimeout: {
+          request: 10000,
+        },
+        body:
+          "grant_type=authorization_code&code=xxx-xxx&redirect_uri=http%3A%2F%2Flocalhost%3A7100%2Foauth2%2Fredirect",
+        method: "POST",
+      },
+      event: "request",
+      level: "error",
+      message:
+        "Failed to retrieve tokens using the OpenID client Timeout awaiting 'request' for 10000ms",
+      stack:
+        "TimeoutError: Timeout awaiting 'request' for 10000ms\n    at ClientRequest.<anonymous> (/home/node/node_modules/got/source/request-as-event-emitter.js:176:14)\n    at Object.onceWrapper (events.js:421:26)\n    at ClientRequest.emit (events.js:326:22)\n    at ClientRequest.EventEmitter.emit (domain.js:483:12)\n    at ClientRequest.origin.emit (/home/node/node_modules/@szmarczak/http-timer/source/index.js:37:11)\n    at Immediate.timeoutHandler (/home/node/node_modules/got/source/utils/timed-out.js:63:11)\n    at processImmediate (internal/timers.js:463:21)",
+      time: "2020-12-08T18:54:51.085Z",
+    };
+    let result = minimalError(testError);
+    expect(result.message).toBe(
+      "Failed to retrieve tokens using the OpenID client Timeout awaiting 'request' for 10000ms"
+    );
+    expect(result.name).toBe("TimeoutError");
+    expect(Object.keys(result)).toHaveLength(2);
   });
 });
