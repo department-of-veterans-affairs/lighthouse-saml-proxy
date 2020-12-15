a172c3dadf212944697ce1a242666879adfce987
lukemarkwordtlibertyits <65039481+lukemarkwordtlibertyits@users.noreply.github.com>
2020-12-15 10:27:40 -0700

    Api 3328 oauth regression script small files (#196)
    * Added Oauth Proxy regression testing script to tests folder
    
    * Added read me for tests
    
    * Removed comment about needing to be signed into docker for the regression tests to work
    
    * Made syntax safer
    
    * Added dependency check
    
    * consistent function naming conventions
    
    * corrected typo
    
    * consistent variable naming conventions
    
    * Corrected expected json for invalid client token handler tests
    
    * Created all tests
    
    * Formatting
    
    * change script types
    
    * Added absolute directory to every file call
    
    * redirect uri is only needed to be assigned once
    
    * Created usage function for assertions
    
    * added usage to intropsect
    
    * Added usage for okta_grants_tests
    
    * added usage to token_tests
    
    * Added usage for regression_tests
    
    * removed unecessary logging
    
    * linting
    
    * Removed temp TOKEN and EXPIRED_TOKEN files
    
    * Removed temp TOKEN and EXPIRED_ACCESS file pattern
    
    * Added checks in regression_tests.sh for global variables
    
    * implemented shorter DIR fetching
    
    * Fixed spelling issue in introspect tests

---
+++ oauth-proxy/tests/scripts/other_tests.sh
@@ -0,0 +1,151 @@
+#!/usr/bin/env bash
+# Team Pivot!
+
+HOST=
+TOKENS=
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pass=1
+curl_status="$(mktemp)"
+curl_body="$(mktemp)"
+
+usage() {
+cat <<EOF
+Tests the variety of minor Oauth Proxy endpoints.
+
+Example
+  ./other_tests.sh --host=https://sandbox-api.va.gov/oauth2 --tokens={ Tokens }
+EOF
+exit 1
+}
+
+for i in "$@"
+do
+case $i in
+    
+    --host=*)
+      HOST="${i#*=}"; shift ;;
+    --tokens=*)
+      TOKENS="${i#*=}"; shift ;;
+    --help|-h)
+      usage ;  exit 1 ;;
+    --) shift ; break ;;
+    *) usage ; exit 1 ;;
+esac
+done
+
+if [[ -z $(echo "$TOKENS" | jq .access_token) ]];
+then
+  echo "ERROR - TOKENS is a required parameter."
+  exit 1
+fi
+
+if [ -z "$HOST" ];
+then
+  echo "ERROR - HOST is a required parameter."
+  exit 1
+fi
+
+# Helper Functions
+
+track_result() {
+  if [[ "$?" -gt 0 ]]
+  then
+    pass=0
+  fi
+}
+
+# --------
+echo -e "\nMisc. Tests"
+
+echo -e "\tRunning ... User Info happy path"
+  
+access_token=$(echo "$TOKENS" | jq ".access_token" | tr -d '"')
+
+curl -s \
+  -H 'Accept: application/json' \
+  -H "Authorization: Bearer ${access_token}" \
+  -w "%{http_code}" \
+  -o "$curl_body" \
+  "$HOST/userinfo" > "$curl_status"
+
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=200
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="sub"
+track_result
+"$DIR"/assertions.sh --has-or-expect-property --json="$(cat "$curl_body")" --property="name" --expected-value="$NAME"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="locale"
+track_result
+"$DIR"/assertions.sh --has-or-expect-property --json="$(cat "$curl_body")" --property="preferred_username" --expected-value="$USER_NAME"
+track_result
+"$DIR"/assertions.sh --has-or-expect-property --json="$(cat "$curl_body")" --property="given_name" --expected-value="$FIRST_NAME"
+track_result
+"$DIR"/assertions.sh --has-or-expect-property --json="$(cat "$curl_body")" --property="middle_name" --expected-value="$MIDDLE_NAME"
+track_result
+"$DIR"/assertions.sh --has-or-expect-property --json="$(cat "$curl_body")" --property="family_name" --expected-value="$LAST_NAME"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="zoneinfo"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="updated_at"
+track_result
+
+echo -e "\tRunning ... Keys happy path"
+
+curl -s \
+  -w "%{http_code}" \
+  -o "$curl_body" \
+  "$HOST/keys" > "$curl_status"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=200
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="keys[0].kty"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="keys[0].alg"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="keys[0].kid"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="keys[0].use"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="keys[0].e"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="keys[0].n"
+track_result
+
+
+echo -e "\tRunning ... Manage happy path"
+
+curl -s \
+  -w "%{http_code}" \
+  -o "$curl_body" \
+  "$HOST/manage" > "$curl_status"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=302
+track_result
+
+echo -e "\tRunning ... Authorize Handler with no state parameter"
+
+if [[ -z $SCOPE ]];
+then 
+  SCOPE="openid%20profile%20disability_rating.read%20service_history.read%20veteran_status.read%20offline_access"
+fi
+
+curl -s \
+  -w "%{http_code}" \
+  -o "$curl_body" \
+  "$HOST/authorization?client_id=$CLIENT_ID&scope=$SCOPE&response_type=code&redirect_uri=$REDIRECT_URI&aud=default" > "$curl_status"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=400
+track_result
+
+"$DIR"/assertions.sh --expect-json --json="$(cat "$curl_body")" --expected-json='{"error": "invalid_request", "error_description": "State parameter required"}'
+track_result
+
+if [[ $pass -lt 1 ]];
+then
+  echo -e "\tFAIL - Some misc. tests did not pass."
+  exit 1
+fi
+
+exit 0
---
+++ oauth-proxy/tests/scripts/token_tests.sh
@@ -0,0 +1,324 @@
+#!/usr/bin/env bash
+# Team Pivot!
+
+HOST=
+CODE=
+export TOKEN=
+export EXPIRED_ACCESS=
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pass=1
+curl_status="$(mktemp)"
+curl_body="$(mktemp)"
+
+usage() {
+cat <<EOF
+Tests the Oauth Proxy's token endpoint.
+
+Example
+  ./token.sh --host=https://sandbox-api.va.gov/oauth2 --code={ CODE }
+EOF
+exit 1
+}
+
+for i in "$@"
+do
+case $i in
+    
+    --host=*)
+      HOST="${i#*=}"; shift ;;
+    --code=*)
+      CODE="${i#*=}"; shift ;;
+    --help|-h)
+      usage ;  exit 1 ;;
+    --) shift ; break ;;
+    *) usage ; exit 1 ;;
+esac
+done
+
+if [ -z "$HOST" ];
+then
+  echo "ERROR - HOST is a required parameter."
+  exit 1
+fi
+
+if [ -z "$CODE" ];
+then
+  echo "ERROR - CODE is a required parameter."
+  exit 1
+fi
+
+# Oauth Proxy functions
+
+do_token() {
+  payload="$1"
+  curl -X POST \
+    -s \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -w "%{http_code}" \
+    -o "$curl_body" \
+    -d "$payload" \
+    "$HOST/TOKEN?redirect_uri=$REDIRECT_URI" > "$curl_status"
+  if [[ "$(cat "$curl_status")" == "200" ]] && [ "$(cat "$curl_body" | jq ".error")" = "null" ];
+  then
+    TOKEN="$(cat "$curl_body")"
+  fi
+}
+
+do_revoke_token() { 
+  local token=$1
+  local grant_type=$2
+  local client_id=$3
+  local client_secret=$4
+  curl -X POST \
+    -s \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -w "%{http_code}" \
+    -o "$curl_body" \
+    -d "grant_type=$grant_type" \
+    -d "token=$token" \
+    -d "client_id=$client_id" \
+    -d "client_secret=$client_secret" \
+    "$HOST/revoke" > "$curl_status"
+}
+
+do_client_credentials() { 
+  local scope=$1
+  local launch=$2
+
+  local url=
+  local network=""
+  if [[ $HOST == *"dev"* ]];
+  then
+    url="https://dev-api.va.gov/oauth2/health-insurance/v1"
+  fi
+
+  if [[ $HOST == *"sandbox"* ]];
+  then
+    url="https://sandbox-api.va.gov/oauth2/health-insurance/v1"
+  fi
+
+  if [[ $HOST == *"localhost"* ]];
+  then
+    url="http://localhost:7100/oauth2/health-insurance/v1"
+    network="-it --network container:oauth-proxy_oauth-proxy_1"
+  fi
+
+  local cc
+  cc="$(docker run $network \
+          vasdvp/lighthouse-auth-utils:latest auth-cc \
+          --client-id="$CC_CLIENT_ID" \
+          --client-secret="$CC_CLIENT_SECRET" \
+          --authorization-url=$url \
+          --scope="$scope" \
+          --launch="$launch")"
+  echo "$cc"
+}
+
+# ----
+
+# Helper Functions
+
+track_result() {
+  if [[ "$?" -gt 0 ]]
+  then
+    pass=0
+  fi
+}
+
+# --------
+echo -e "\nToken Tests"
+
+echo -e "\tRunning ... Token Handler code happy path"
+
+do_token "$(jq \
+                -scn \
+                --arg client_id "$CLIENT_ID" \
+                --arg grant_type "authorization_code" \
+                --arg code "$CODE" \
+                --arg secret "$CLIENT_SECRET" \
+                '{"client_id": $client_id, "grant_type": $grant_type, "code": $code, "client_secret": $secret}')"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=200
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="access_token"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="id_token"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="refresh_token"
+track_result
+"$DIR"/assertions.sh --expect-property --json="$(cat "$curl_body")" --property="token_type" --expected-value="Bearer"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="scope"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="expires_in"
+track_result
+
+if [[ "$(cat "$curl_body" | jq .scope)" == *"launch/patient"* ]];
+then
+  "$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="patient"
+  track_result
+fi
+
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="state"
+track_result
+
+echo -e "\tRunning ... Token Handler expired code"
+
+do_token "$(jq \
+                -scn \
+                --arg client_id "$CLIENT_ID" \
+                --arg grant_type "authorization_code" \
+                --arg code "$CODE" \
+                --arg secret "$CLIENT_SECRET" \
+                '{"client_id": $client_id, "grant_type": $grant_type, "code": $code, "client_secret": $secret}')"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=400
+track_result
+"$DIR"/assertions.sh --expect-json --json="$(cat "$curl_body")" --expected-json='{"error": "invalid_grant", "error_description": "The authorization code is invalid or has expired."}'
+track_result
+
+echo -e "\tRunning ... Token Handler invalid code"
+
+do_token "$(jq \
+                -scn \
+                --arg client_id "$CLIENT_ID" \
+                --arg grant_type "authorization_code" \
+                --arg code "Invalid" \
+                --arg secret "$CLIENT_SECRET" \
+                '{"client_id": $client_id, "grant_type": $grant_type, "code": $code, "client_secret": $secret}')"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=400
+track_result
+"$DIR"/assertions.sh --expect-json --json="$(cat "$curl_body")" --expected-json='{"error": "invalid_grant", "error_description": "The authorization code is invalid or has expired."}'
+track_result
+
+echo -e "\tRunning ... Token Handler code path invalid client id"
+
+do_token "$(jq \
+                -scn \
+                --arg client_id "Invalid" \
+                --arg grant_type "authorization_code" \
+                --arg code "$CODE" \
+                --arg secret "$CLIENT_SECRET" \
+                '{"client_id": $client_id, "grant_type": $grant_type, "code": $code, "client_secret": $secret}')"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=401
+track_result
+
+# expect_json_body '{"error":"invalid_client", "error_description": "Invalid value for client_id parameter."}'
+
+echo -e "\tRunning ... Revoke active token happy path"
+
+access_token=$(echo "$TOKEN" | jq ".access_token" | tr -d '"')
+do_revoke_token "$access_token" "access_token" "$CLIENT_ID" "$CLIENT_SECRET"
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=200
+track_result
+
+EXPIRED_ACCESS="$access_token"
+
+# The access TOKEN is now expired
+
+echo -e "\tRunning ... Token Handler refresh happy path"
+refresh=$(echo "$TOKEN" | jq ".refresh_token" | tr -d '"')
+
+do_token "$(jq \
+              -scn \
+              --arg client_id "$CLIENT_ID" \
+              --arg grant_type "refresh_token" \
+              --arg refresh_token "$refresh" \
+              --arg secret "$CLIENT_SECRET" \
+              '{"client_id": $client_id, "grant_type": $grant_type, "refresh_token": $refresh_token, "client_secret": $secret}')"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=200
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="access_token"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="id_token"
+track_result
+"$DIR"/assertions.sh --expect-property --json="$(cat "$curl_body")" --property="refresh_token" --expected-value="$refresh"
+track_result
+"$DIR"/assertions.sh --expect-property --json="$(cat "$curl_body")" --property="token_type" --expected-value="Bearer"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="scope"
+track_result
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="expires_in"
+track_result
+
+if [[ "$(cat "$curl_body" | jq .scope)" == *"launch/patient"* ]];
+then
+  "$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="patient"
+  track_result
+fi
+
+"$DIR"/assertions.sh --has-property --json="$(cat "$curl_body")" --property="state"
+track_result
+
+echo -e "\tRunning ... Token Handler invalid refresh Token"
+
+do_token "$(jq \
+              -scn \
+              --arg client_id "$CLIENT_ID" \
+              --arg grant_type "refresh_token" \
+              --arg refresh_token "Invalid" \
+              --arg secret "$CLIENT_SECRET" \
+              '{"client_id": $client_id, "grant_type": $grant_type, "refresh_token": $refresh_token, "client_secret": $secret}')"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=400
+track_result
+"$DIR"/assertions.sh --expect-json --json="$(cat "$curl_body")" --expected-json='{"error": "invalid_grant", "error_description": "The refresh token is invalid or expired."}'
+track_result
+
+echo -e "\tRunning ... Token Handler refresh path invalid client id"
+  
+do_token "$(jq \
+              -scn \
+              --arg client_id "Invalid" \
+              --arg grant_type "refresh_token" \
+              --arg refresh_token "$(echo "$TOKEN" | jq ".refresh_token" | tr -d '"')" \
+              --arg secret "$CLIENT_SECRET" \
+              '{"client_id": $client_id, "grant_type": $grant_type, "refresh_token": $refresh_token, "client_secret": $secret}')"
+
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=401
+track_result
+# expect_json_body '{"error":"invalid_client", "error_description": "Invalid value for client_id parameter."}'
+
+echo -e "\tRunning ... Client Credentials happy path"
+
+cc=$(do_client_credentials "launch/patient" "123V456")
+
+"$DIR"/assertions.sh --has-property --json=$cc --property="access_token"
+track_result
+"$DIR"/assertions.sh --expect-property --json="$cc" --property="token_type" --expected-value="Bearer"
+track_result
+"$DIR"/assertions.sh --expect-property --json="$cc" --property="scope" --expected-value="launch/patient"
+track_result
+"$DIR"/assertions.sh --has-property --json="$cc" --property="expires_in"
+track_result
+
+echo -e "\tRunning ... Token Handler invalid strategy"
+
+do_token "$(jq \
+                -scn \
+                --arg client_id "$CLIENT_ID" \
+                --arg grant_type "invalid" \
+                --arg secret "$CLIENT_SECRET" \
+                '{"client_id": $client_id, "grant_type": $grant_type, "client_secret": $secret}')"
+
+"$DIR"/assertions.sh --expect-status --status="$(cat "$curl_status")" --expected-status=400
+track_result
+"$DIR"/assertions.sh --expect-json --json="$(cat "$curl_body")" --expected-json='{"error":"unsupported_grant_type","error_description":"Only authorization and refresh_token grant types are supported"}'
+track_result
+
+# It is not feasible to test Client Credential edge cases yet.
+
+if [[ $pass -lt 1 ]];
+then
+  echo -e "\tFAIL - Some Token tests did not pass."
+  return 1
+fi
+
+return 0
