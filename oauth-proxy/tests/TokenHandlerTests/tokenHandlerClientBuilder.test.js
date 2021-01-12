e7a442cf080043138bb4fee8d6ea9b5271431561
Luke Markwordt <luke.markwordt@libertyits.com>
2021-01-12 15:22:14 -0700

    Test outline for tokenHandlerClientBuilder.test.js

---
+++ oauth-proxy/tests/TokenHandlerTests/tokenHandlerClientBuilder.test.js
@@ -0,0 +1,17 @@
+require("jest");
+
+describe("buildTokenHandlerClient Tests", () => {
+  it("Refresh Client Basic Auth", () => {})
+
+  it("Refresh Client Body Auth", () => {})
+
+  it("Code Client PKCE Auth", () => {})
+
+  it("ClientCredentials Client", () => {})
+
+  it("Unsupported Grant Client", () => {})
+
+  it("Code Client Invalid Auth", () => {})
+
+  it("ClientCredentials Client Invalid Assertion Type", () => {})
+})
