0f7ca7924441a3536ddb166606604bc7334db877
Luke Markwordt <luke.markwordt@libertyits.com>
2021-01-12 16:01:59 -0700

    Implemented all tokenHandlerTestUtils.js methods

--- oauth-proxy/tests/TokenHandlerTests/tokenHandlerClientBuilder.test.js
+++ oauth-proxy/tests/TokenHandlerTests/tokenHandlerClientBuilder.test.js
@@ -1,17 +1,17 @@
 require("jest");
 
 describe("buildTokenHandlerClient Tests", () => {
-  it("Refresh Client Basic Auth", () => {})
+  it("Refresh Client Basic Auth", () => {});
 
-  it("Refresh Client Body Auth", () => {})
+  it("Refresh Client Body Auth", () => {});
 
-  it("Code Client PKCE Auth", () => {})
+  it("Code Client PKCE Auth", () => {});
 
-  it("ClientCredentials Client", () => {})
+  it("ClientCredentials Client", () => {});
 
-  it("Unsupported Grant Client", () => {})
+  it("Unsupported Grant Client", () => {});
 
-  it("Code Client Invalid Auth", () => {})
+  it("Code Client Invalid Auth", () => {});
 
-  it("ClientCredentials Client Invalid Assertion Type", () => {})
-})
+  it("ClientCredentials Client Invalid Assertion Type", () => {});
+});
