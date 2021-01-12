e4059e72a173676843916e0ed321fd744177bd42
Luke Markwordt <luke.markwordt@libertyits.com>
2021-01-12 15:08:02 -0700

    Test outline for tokenHandlerClient.test.js

---
+++ oauth-proxy/tests/TokenHandlerTests/tokenHandlerClient.test.js
@@ -0,0 +1,16 @@
+const { IoT1ClickDevicesService } = require("aws-sdk");
+const { describe } = require("yargs");
+
+require("jest");
+
+describe( "handleToken tests", () => {
+  it("Happy Path Static", () => {})
+
+  it("Happy Path no launch/patient", () => {})
+
+  it("Happy Path with launch/patient", () => {})
+
+  it("getToken 401 Response", () => {})
+
+  it("getToken 500 Response", () => {})
+})
