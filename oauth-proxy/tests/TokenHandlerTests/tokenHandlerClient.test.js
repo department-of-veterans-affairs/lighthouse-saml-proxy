faa058e61b93cc5cced1f40bc245c7df59851bdb
Luke Markwordt <luke.markwordt@libertyits.com>
2021-01-12 16:01:59 -0700

    Implemented all tokenHandlerTestUtils.js methods

--- oauth-proxy/tests/TokenHandlerTests/tokenHandlerClient.test.js
+++ oauth-proxy/tests/TokenHandlerTests/tokenHandlerClient.test.js
@@ -1,13 +1,13 @@
 require("jest");
 
-describe( "handleToken tests", () => {
-  it("Happy Path Static", () => {})
+describe("handleToken tests", () => {
+  it("Happy Path Static", () => {});
 
-  it("Happy Path no launch/patient", () => {})
+  it("Happy Path no launch/patient", () => {});
 
-  it("Happy Path with launch/patient", () => {})
+  it("Happy Path with launch/patient", () => {});
 
-  it("getToken 401 Response", () => {})
+  it("getToken 401 Response", () => {});
 
-  it("getToken 500 Response", () => {})
-})
+  it("getToken 500 Response", () => {});
+});
--- oauth-proxy/tests/TokenHandlerTests/tokenHandlerTestUtils.js
+++ oauth-proxy/tests/TokenHandlerTests/tokenHandlerTestUtils.js
@@ -1,22 +1,54 @@
-const buildGetTokenResponseStrategy = () => {
+const buildGetTokenResponseStrategy = (response, error = false) => {
+  const getTokenResponseStrategy = { getTokenResponse: jest.fn() };
+  getTokenResponseStrategy.getTokenResponse.mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      if (!error) {
+        resolve(response);
+      } else {
+        reject(response);
+      }
+    });
+  });
+  return getTokenResponseStrategy;
+};
 
-}
-
-const buildGetDocumentStrategy = () => {
-
-}
+const buildGetDocumentStrategy = (document) => {
+  const getDocumentStrategy = { pullDocumentFromDynamo: jest.fn() };
+  getDocumentStrategy.pullDocumentFromDynamo.mockImplementation(() => {
+    return new Promise((resolve) => {
+      resolve(document);
+    });
+  });
+  return getDocumentStrategy;
+};
 
 const buildSaveDocumentStrategy = () => {
+  const saveDocumentStrategy = { saveDocumentToDynamo: jest.fn() };
+  saveDocumentStrategy.saveDocumentToDynamo.mockImplementation(() => {
+    return new Promise((resolve) => {
+      resolve();
+    });
+  });
+  return saveDocumentStrategy;
+};
 
-}
-
-const buildGetPatientInfoStrategy = () => {
-
-}
+const buildGetPatientInfoStrategy = (patient, error = false) => {
+  const getPatientInfoStrategy = { createPatientInfo: jest.fn() };
+  getPatientInfoStrategy.createPatientInfo.mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      if (!error) {
+        resolve(patient);
+      } else {
+        reject(patient);
+      }
+    });
+  });
+  return getPatientInfoStrategy;
+};
 
 module.exports = {
   buildGetTokenResponseStrategy,
   buildGetDocumentStrategy,
   buildSaveDocumentStrategy,
-  buildGetPatientInfoStrategy
-}
+  buildGetPatientInfoStrategy,
+};
