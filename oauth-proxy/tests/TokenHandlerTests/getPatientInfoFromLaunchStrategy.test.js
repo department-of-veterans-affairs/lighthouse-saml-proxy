8dbcff5d651b8571da99eeff9dd52bae3419d783
Luke Markwordt <luke.markwordt@libertyits.com>
2021-01-13 16:51:19 -0700

    fixed getPatientInfoFromLaunchStrategy tests

--- oauth-proxy/tests/TokenHandlerTests/getPatientInfoFromLaunchStrategy.test.js
+++ oauth-proxy/tests/TokenHandlerTests/getPatientInfoFromLaunchStrategy.test.js
@@ -5,10 +5,10 @@ const {
 require("jest");
 
 describe("createPatientInfo Tests", () => {
-  it("Happy Path", () => {
-    let response = new GetPatientInfoFromLaunchStrategy({
+  it("Happy Path", async () => {
+    let response = await new GetPatientInfoFromLaunchStrategy({
       body: { launch: "launch" },
-    });
+    }).createPatientInfo();
     expect(response).toBe("launch");
   });
 });
