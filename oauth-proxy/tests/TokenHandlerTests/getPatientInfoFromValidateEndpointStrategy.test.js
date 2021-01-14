ec9ba74cf03a452ff6a03a932660aa7e2f6f0d96
Luke Markwordt <luke.markwordt@libertyits.com>
2021-01-13 17:12:04 -0700

    All getPatientInfoFromValidateEndpointStrategy implementation

--- oauth-proxy/tests/TokenHandlerTests/getPatientInfoFromValidateEndpointStrategy.test.js
+++ oauth-proxy/tests/TokenHandlerTests/getPatientInfoFromValidateEndpointStrategy.test.js
@@ -1,7 +1,67 @@
 require("jest");
 
-describe("createPatientInfo Tests", () => {
-  it("Happy Path", async () => {});
+const {
+  GetPatientInfoFromValidateEndpointStrategy,
+} = require("../../oauthHandlers/tokenHandlerStrategyClasses/getPatientInfoStrategies/getPatientInfoFromValidateEndpointStrategy");
+const {
+  buildFakeLogger,
+} = require("../testUtils");
+const {
+  buildValidateToken
+} = require("./tokenHandlerTestUtils");
+describe("getPatientInfoFromValidateEndpointStrategy tests", () => {
+  let logger;
+  let mockValidate;
 
-  it("Validation Error", async () => {});
+  beforeEach(() => {
+    logger = buildFakeLogger();
+  });
+  it("Happy Path", async () => {
+    mockValidate = buildValidateToken({va_identifiers: {icn: "patient"}}, false);
+    let strategy = new GetPatientInfoFromValidateEndpointStrategy(
+      mockValidate,
+      logger
+    );
+    let response = await strategy.createPatientInfo(
+        { access_token: "token" },
+        { aud: "aud" }
+      );
+    expect(response).toBe("patient")
+  })
+  it("Type Error", async () => {
+    mockValidate = buildValidateToken({}, true)
+    let strategy = new GetPatientInfoFromValidateEndpointStrategy(
+      mockValidate,
+      logger
+    );
+    try {
+      await strategy.createPatientInfo(null, null);
+    } catch (err) {
+      expect(true).toBe(true);
+      return;
+    }
+    expect(true).toBe(false);
+  });
+
+  it("Validate error", async () => {
+    mockValidate = buildValidateToken({}, true);
+    let strategy = new GetPatientInfoFromValidateEndpointStrategy(
+      mockValidate,
+      logger
+    );
+    try {
+      await strategy.createPatientInfo(
+        { access_token: "token" },
+        { aud: "aud" }
+      );
+    } catch (err) {
+      expect(true).toBe(true);
+      expect(err.status).toBe(500);
+      expect(logger.error).toHaveBeenCalledWith(
+        "Invalid grant, could not find a valid patient identifier for the provided authorization code."
+      );
+      return;
+    }
+    expect(true).toBe(false);
+  });
 });
