15abd9a3fea257800c73458ac218260309eb2229
Larry Crochet <larry.crochet@libertyits.com>
2021-01-14 14:50:27 -0500

    pulldocument stategies use new method

--- oauth-proxy/oauthHandlers/tokenHandlerStrategyClasses/tokenHandlerClient.js
+++ oauth-proxy/oauthHandlers/tokenHandlerStrategyClasses/tokenHandlerClient.js
@@ -59,7 +59,7 @@ class TokenHandlerClient {
         document,
         tokens
       );
-      state = (document.state && document.state.S) || null;
+      state = (document.state) || null;
     }
     state = state || null;
 
