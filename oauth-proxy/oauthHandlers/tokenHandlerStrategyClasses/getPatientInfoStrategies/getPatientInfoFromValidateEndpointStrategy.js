709a6f297bc3fcbeedfb9622a897c898cc8ae0ff
Luke Markwordt <luke.markwordt@libertyits.com>
2021-01-26 15:09:42 -0700

    validate error throws a statusCode not status

--- oauth-proxy/oauthHandlers/tokenHandlerStrategyClasses/getPatientInfoStrategies/getPatientInfoFromValidateEndpointStrategy.js
+++ oauth-proxy/oauthHandlers/tokenHandlerStrategyClasses/getPatientInfoStrategies/getPatientInfoFromValidateEndpointStrategy.js
@@ -25,7 +25,7 @@ class GetPatientInfoFromValidateEndpointStrategy {
         "Invalid grant, could not find a valid patient identifier for the provided authorization code."
       );
       throw {
-        status: 503,
+        statusCode: 503,
       };
     }
     return patient;
