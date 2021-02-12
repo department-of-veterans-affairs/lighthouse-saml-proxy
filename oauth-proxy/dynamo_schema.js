e836f0bf6becb64538f6f654fc61f712998436e2
Larry Crochet <larry.crochet@libertyits.com>
2021-02-11 19:08:38 -0500

    update test client

--- oauth-proxy/dynamo_schema.js
+++ oauth-proxy/dynamo_schema.js
@@ -238,7 +238,10 @@ function createTestClientEntry() {
     TableName: "Clients",
     Item: {
       client_id: { S: "testclient1" },
-      redirect_uris: { SS: ["http://localhost:8080/auth/cb", "http://localhost:18080/auth/cb"] },
+      redirect_uris: {
+        SS: ["http://localhost:8080/auth/cb", "http://localhost:18080/auth/cb"],
+      },
+      system: { S: "oauthi" },
     },
   };
   dynamo.putItem(itemParams, (err, data) => {
