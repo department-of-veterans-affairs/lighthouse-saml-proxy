880e05d612b1e7a45b0dfab3d05745f0dbcd508b
Luke Markwordt <luke.markwordt@libertyits.com>
2021-03-01 12:42:14 -0700

    Created test shell for issuer_helper

---
+++ oauth-proxy/tests/issuer_helper.test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+require("jest");
+
+describe('createIssuer tests', () => {
+  it('Happy Path no custom endpoints', async () => {
+    fail('not implemented')
+  })
+
+  it('Happy Path all custom endpoints', async () => {
+    fail('not implemented')
+  })
+
+  it('Error in issuer discover', async () => {
+    fail('not implemented')
+  })
+})
