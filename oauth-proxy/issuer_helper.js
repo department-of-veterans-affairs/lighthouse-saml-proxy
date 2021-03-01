8a9a0c4e584abb269de62106299c95f69bfbba3c
Luke Markwordt <luke.markwordt@libertyits.com>
2021-03-01 15:06:33 -0700

    Implemented issuer_helper

--- oauth-proxy/issuer_helper.js
+++ oauth-proxy/issuer_helper.js
@@ -1,3 +1,25 @@
-const createIssuer = () => {}
+const { Issuer } = require("openid-client");
 
-module.exports = createIssuer;
+const createIssuer = async (serviceConfig) => {
+  let discovered_issuer = await Issuer.discover(serviceConfig.upstream_issuer);
+  if (serviceConfig.custom_metadata) {
+    new Issuer(
+      overrideMetadata(
+        serviceConfig.custom_metadata,
+        discovered_issuer.metadata
+      )
+    );
+  }
+  return discovered_issuer;
+};
+
+const overrideMetadata = (serviceConfig, discover_metadata) => {
+  Object.entries(serviceConfig).forEach(([key, value]) => {
+    if (value) {
+      discover_metadata[key] = value;
+    }
+  });
+  return discover_metadata;
+};
+
+module.exports = { createIssuer };
