6bcde487924f13e2a042ecb58264cc06425d06ca
bdgrider <benjamin.grider@libertyits.com>
2021-03-17 15:30:03 -0400

    Removing entrypoint script that is no longer being used.

--- oauth-proxy/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -n "$CUSTOM_CERTS" ]; then
-  mkdir -p /tmp/customcerts/
-  aws ssm get-parameters --names $CUSTOM_CERTS | jq -r '.Parameters[0].Value' > /tmp/customcerts/certs.cer
-  cat /tmp/customcerts/certs.cer >> /ca-certificates/ca-certs.pem
-fi
-
-exec "$@"
