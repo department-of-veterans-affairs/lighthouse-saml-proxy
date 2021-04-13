1559dd682ad7a35f37bcbf347caea7594d053be9
Luke Markwordt <luke.markwordt@libertyits.com>
2021-04-13 12:09:34 -0600

    removed OAuthRequests from dynamo_schema

--- oauth-proxy/dynamo_schema.js
+++ oauth-proxy/dynamo_schema.js
@@ -30,68 +30,6 @@ const dynamo = new DynamoDB({ endpoint });
 
 let tableParams = {
   AttributeDefinitions: [
-    { AttributeName: "state", AttributeType: "S" },
-    { AttributeName: "code", AttributeType: "S" },
-    { AttributeName: "refresh_token", AttributeType: "S" },
-  ],
-  KeySchema: [{ AttributeName: "state", KeyType: "HASH" }],
-  ProvisionedThroughput: {
-    ReadCapacityUnits: 10,
-    WriteCapacityUnits: 10,
-  },
-  GlobalSecondaryIndexes: [
-    {
-      IndexName: "oauth_code_index",
-      KeySchema: [
-        {
-          AttributeName: "code",
-          KeyType: "HASH",
-        },
-      ],
-      Projection: {
-        ProjectionType: "ALL",
-      },
-      ProvisionedThroughput: {
-        ReadCapacityUnits: 10,
-        WriteCapacityUnits: 10,
-      },
-    },
-    {
-      IndexName: "oauth_refresh_token_index",
-      KeySchema: [
-        {
-          AttributeName: "refresh_token",
-          KeyType: "HASH",
-        },
-      ],
-      Projection: {
-        ProjectionType: "ALL",
-      },
-      ProvisionedThroughput: {
-        ReadCapacityUnits: 10,
-        WriteCapacityUnits: 10,
-      },
-    },
-  ],
-  TableName: "OAuthRequests",
-};
-
-dynamo.createTable(tableParams, (err, data) => {
-  if (err) {
-    console.error(
-      "Unable to create table. Error JSON:",
-      JSON.stringify(err, null, 2)
-    );
-  } else {
-    console.log(
-      "Created table. Table description JSON:",
-      JSON.stringify(data, null, 2)
-    );
-  }
-});
-
-tableParams = {
-  AttributeDefinitions: [
     { AttributeName: "internal_state", AttributeType: "S" },
     { AttributeName: "state", AttributeType: "S" },
     { AttributeName: "code", AttributeType: "S" },
