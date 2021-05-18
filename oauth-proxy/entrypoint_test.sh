1e1ef981ca438afadf2bdd8759e384922fbacb2c
Luke Markwordt <luke.markwordt@libertyits.com>
2021-05-18 14:38:52 -0600

    Created entrypoint_test.sh file

--- oauth-proxy/entrypoint_test.sh
+++ oauth-proxy/entrypoint_test.sh
@@ -21,3 +21,38 @@ if [ $# -eq 0 ]; then
   usage
   exit 1
 fi
+
+USER_EMAIL=
+USER_PASSWORD=
+CLIENT_ID=
+CLIENT_SECRET=
+CC_CLIENT_ID=
+CC_CLIENT_SECRET=
+HOST=
+TEST_CLAIMS=
+
+for i in "$@"
+do
+case $i in
+    --help|-h)
+      usage; exit 1 ;;
+    --test-claims)
+      TEST_CLAIMS="--test-claims"; shift ;;
+    --user-email=*)
+      export USER_EMAIL="${i#*=}"; shift ;;
+    --user-password=*)
+      export USER_PASSWORD="${i#*=}"; shift ;;
+    --client-id=*)
+      export CLIENT_ID="${i#*=}"; shift ;;
+    --client-secret*)
+      export CLIENT_SECRET="${i#*=}"; shift ;;
+    --cc-client-id*)
+      export CC_CLIENT_ID="${i#*=}"; shift ;;
+    --cc-client-secret*)
+      export CC_CLIENT_SECRET="${i#*=}"; shift ;;
+    --host*)
+      export HOST="${i#*=}"; shift ;;
+esac
+done
+
+./bats/regression_tests.sh $TEST_CLAIMS
