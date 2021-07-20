0c1bde80f8ab59b6430a1cdfd335c01ac712a24d
Larry Crochet <larry.crochet@libertyits.com>
2021-07-20 15:14:39 -0400

    lint

--- src/routes/handlers.test.ts
+++ src/routes/handlers.test.ts
@@ -153,74 +153,44 @@ describe("samlLogin", () => {
     mockNext = jest.fn();
   });
   afterAll(() => {
-    // console.log("The end");
-    // expect(mockResponse.render).toHaveBeenCalledWith("login_selection", {
-    //   id_me_login_link:
-    //     "https://identityProviderUrl.com?SAMLRequest=fVDBqsJADDz7F2Xvte2KCMEWFA8KiuXZ9w7e1rroQputm1T0711XD76LOU3IzCSTKam26WDW8xl%2F9KXXxNGtbZAgDHLROwSryBCgajUB17CbbdYghyl0zrKtbSMiX4OF1xpUbCzm4szcESSJOWpkw%2FfS2avH7tc1w9q2QbFa5OIFiHq9QmKFnAuZyixOZZxNqmwE4xRktg%2B08r1ubvBo8PT9tsOLRLCsqjIut7tKRH%2FaUbjOE0QxfSaEsNt9ZP5uq4i0e0b0%2BuTD4N39%2F2TxAA%3D%3D&RelayState=",
-    //   dslogon_login_link:
-    //     "https://identityProviderUrl.com?SAMLRequest=fVDBbsIwDD3zF1XupW3QNMmiSJ04DImJCsoO3LI2GpFap8QuGn9PSDl0F3x6lt979vOSVNf2UAx8xr2%2BDJo4%2ButaJAiDXAwOwSoyBKg6TcA1HIqvLch5Cr2zbGvbisjXbO21BhUbi7k4M%2FcESWIajWz4Vjp79dgdXTuvbRcUm3UuRkA06A0SK%2BRcyFRmcSrj7L3KFvCWglycAq18rvsw2Bj8fX3bz0gi%2BKyqMi53h0pE39pRuM4TxGr5SAhht5tkfm2riLR7RPT6ZGLw7P5%2FcnUH&RelayState=",
-    //   mhv_login_link:
-    //     "https://identityProviderUrl.com?SAMLRequest=fVDBbsIwDD3zF1XupW3QNMmiSJ04DImJCsoO3LI2GpFap8QuGn9PSDl0F3x6lt979vOSVNf2UAx8xr2%2BDJo4%2ButaJAiDXAwOwSoyBKg6TcA1HIqvLch5Cr2zbGvbisjXbO21BhUbi7k4M%2FcESWIajWz4Vjp79dgdXTuvbRcUm3UuRkA06A0SK%2BRcyFRmcSrj7L3KFvCWglycAq18rvsw2Bj8fX3bz0gi%2BKyqMi53h0pE39pRuM4TxGr5SAhht5tkfm2riLR7RPT6ZGLw7P5%2FcnUH&RelayState=",
-    //   id_me_signup_link:
-    //     "https://identityProviderUrl.com?SAMLRequest=fVDBbsIwDD3zF1XupW3QNMmiSJ04DImJCsoO3LI2GpFap8QuGn9PSDl0F3x6lt979vOSVNf2UAx8xr2%2BDJo4%2ButaJAiDXAwOwSoyBKg6TcA1HIqvLch5Cr2zbGvbisjXbO21BhUbi7k4M%2FcESWIajWz4Vjp79dgdXTuvbRcUm3UuRkA06A0SK%2BRcyFRmcSrj7L3KFvCWglycAq18rvsw2Bj8fX3bz0gi%2BKyqMi53h0pE39pRuM4TxGr5SAhht5tkfm2riLR7RPT6ZGLw7P5%2FcnUH&RelayState=&op=signup",
-    // });
-    // expect(mockResponse.render).toHaveBeenCalledWith("verify", {
-    //   id_me_login_link:
-    //     "https://identityProviderUrl.com?SAMLRequest=fVDBqsJADDz7F2Xvte2KCMEWFA8KiuXZ9w7e1rroQputm1T0711XD76LOU3IzCSTKam26WDW8xl%2F9KXXxNGtbZAgDHLROwSryBCgajUB17CbbdYghyl0zrKtbSMiX4OF1xpUbCzm4szcESSJOWpkw%2FfS2avH7tc1w9q2QbFa5OIFiHq9QmKFnAuZyixOZZxNqmwE4xRktg%2B08r1ubvBo8PT9tsOLRLCsqjIut7tKRH%2FaUbjOE0QxfSaEsNt9ZP5uq4i0e0b0%2BuTD4N39%2F2TxAA%3D%3D&RelayState=",
-    //   dslogon_login_link:
-    //     "https://identityProviderUrl.com?SAMLRequest=fVDBbsIwDD3zF1XupW3QNMmiSJ04DImJCsoO3LI2GpFap8QuGn9PSDl0F3x6lt979vOSVNf2UAx8xr2%2BDJo4%2ButaJAiDXAwOwSoyBKg6TcA1HIqvLch5Cr2zbGvbisjXbO21BhUbi7k4M%2FcESWIajWz4Vjp79dgdXTuvbRcUm3UuRkA06A0SK%2BRcyFRmcSrj7L3KFvCWglycAq18rvsw2Bj8fX3bz0gi%2BKyqMi53h0pE39pRuM4TxGr5SAhht5tkfm2riLR7RPT6ZGLw7P5%2FcnUH&RelayState=",
-    //   mhv_login_link:
-    //     "https://identityProviderUrl.com?SAMLRequest=fVDBbsIwDD3zF1XupW3QNMmiSJ04DImJCsoO3LI2GpFap8QuGn9PSDl0F3x6lt979vOSVNf2UAx8xr2%2BDJo4%2ButaJAiDXAwOwSoyBKg6TcA1HIqvLch5Cr2zbGvbisjXbO21BhUbi7k4M%2FcESWIajWz4Vjp79dgdXTuvbRcUm3UuRkA06A0SK%2BRcyFRmcSrj7L3KFvCWglycAq18rvsw2Bj8fX3bz0gi%2BKyqMi53h0pE39pRuM4TxGr5SAhht5tkfm2riLR7RPT6ZGLw7P5%2FcnUH&RelayState=",
-    //   id_me_signup_link:
-    //     "https://identityProviderUrl.com?SAMLRequest=fVDBbsIwDD3zF1XupW3QNMmiSJ04DImJCsoO3LI2GpFap8QuGn9PSDl0F3x6lt979vOSVNf2UAx8xr2%2BDJo4%2ButaJAiDXAwOwSoyBKg6TcA1HIqvLch5Cr2zbGvbisjXbO21BhUbi7k4M%2FcESWIajWz4Vjp79dgdXTuvbRcUm3UuRkA06A0SK%2BRcyFRmcSrj7L3KFvCWglycAq18rvsw2Bj8fX3bz0gi%2BKyqMi53h0pE39pRuM4TxGr5SAhht5tkfm2riLR7RPT6ZGLw7P5%2FcnUH&RelayState=&op=signup",
-    // });
+    expect(mockResponse.render).toHaveBeenCalledWith("verify", {
+      id_me_login_link:
+        "https://identityProviderUrl.com?SAMLRequest=fVDBqsJADDz7F2Xvte2KCMEWFA8KiuXZ9w7e1rroQputm1T0711XD76LOU3IzCSTKam26WDW8xl%2F9KXXxNGtbZAgDHLROwSryBCgajUB17CbbdYghyl0zrKtbSMiX4OF1xpUbCzm4szcESSJOWpkw%2FfS2avH7tc1w9q2QbFa5OIFiHq9QmKFnAuZyixOZZxNqmwE4xRktg%2B08r1ubvBo8PT9tsOLRLCsqjIut7tKRH%2FaUbjOE0QxfSaEsNt9ZP5uq4i0e0b0%2BuTD4N39%2F2TxAA%3D%3D&RelayState=",
+      dslogon_login_link:
+        "https://identityProviderUrl.com?SAMLRequest=fVDBbsIwDD3zF1XupW3QNMmiSJ04DImJCsoO3LI2GpFap8QuGn9PSDl0F3x6lt979vOSVNf2UAx8xr2%2BDJo4%2ButaJAiDXAwOwSoyBKg6TcA1HIqvLch5Cr2zbGvbisjXbO21BhUbi7k4M%2FcESWIajWz4Vjp79dgdXTuvbRcUm3UuRkA06A0SK%2BRcyFRmcSrj7L3KFvCWglycAq18rvsw2Bj8fX3bz0gi%2BKyqMi53h0pE39pRuM4TxGr5SAhht5tkfm2riLR7RPT6ZGLw7P5%2FcnUH&RelayState=",
+      mhv_login_link:
+        "https://identityProviderUrl.com?SAMLRequest=fVDBbsIwDD3zF1XupW3QNMmiSJ04DImJCsoO3LI2GpFap8QuGn9PSDl0F3x6lt979vOSVNf2UAx8xr2%2BDJo4%2ButaJAiDXAwOwSoyBKg6TcA1HIqvLch5Cr2zbGvbisjXbO21BhUbi7k4M%2FcESWIajWz4Vjp79dgdXTuvbRcUm3UuRkA06A0SK%2BRcyFRmcSrj7L3KFvCWglycAq18rvsw2Bj8fX3bz0gi%2BKyqMi53h0pE39pRuM4TxGr5SAhht5tkfm2riLR7RPT6ZGLw7P5%2FcnUH&RelayState=",
+      id_me_signup_link:
+        "https://identityProviderUrl.com?SAMLRequest=fVDBbsIwDD3zF1XupW3QNMmiSJ04DImJCsoO3LI2GpFap8QuGn9PSDl0F3x6lt979vOSVNf2UAx8xr2%2BDJo4%2ButaJAiDXAwOwSoyBKg6TcA1HIqvLch5Cr2zbGvbisjXbO21BhUbi7k4M%2FcESWIajWz4Vjp79dgdXTuvbRcUm3UuRkA06A0SK%2BRcyFRmcSrj7L3KFvCWglycAq18rvsw2Bj8fX3bz0gi%2BKyqMi53h0pE39pRuM4TxGr5SAhht5tkfm2riLR7RPT6ZGLw7P5%2FcnUH&RelayState=&op=signup",
+    });
   });
 
   it("Happy Path", async () => {
     mockRequest.authnRequest = {
       relayState: "theRelayState",
     };
-    const expectedAuthOpts = {
-      id_me_login_link:
-        "https://identityProviderUrl.com?SAMLRequest=fVBND8FAED37F83eqx8kYqIS4kBCNJSD26oNm7SztTMV%2Fr21HLiY05vMe2%2FmzYhkXTUwafmCG3VtFXFwrysk8INMtBbBSNIEKGtFwCVsJ6slpN0YGmvYlKYSgavOzGk1StYGM3FhbgiiSJ8UsuZHbs3NYbuzVbc0tVcsZpl4A6JWLZBYImcijdMkjAdhMizSBHo9iPsHT8s%2F66YaTxrP%2F287vkkE86LIw3y9LUSwV5b8dY4gxqNXQvC77Vfm%2F7aSSNlXRKePvgw%2B3e8nx08%3D&RelayState=",
-      dslogon_login_link:
-        "https://identityProviderUrl.com?SAMLRequest=fVBND8FAED37F83eqx8kYqIS4kBCNJSD26oNm7SztTMV%2Fr21HLiY05vMe2%2FmzYhkXTUwafmCG3VtFXFwrysk8INMtBbBSNIEKGtFwCVsJ6slpN0YGmvYlKYSgavOzGk1StYGM3FhbgiiSJ8UsuZHbs3NYbuzVbc0tVcsZpl4A6JWLZBYImcijdMkjAdhMizSBHo9iPsHT8s%2F66YaTxrP%2F287vkkE86LIw3y9LUSwV5b8dY4gxqNXQvC77Vfm%2F7aSSNlXRKePvgw%2B3e8nx08%3D&RelayState=",
-      mhv_login_link:
-        "https://identityProviderUrl.com?SAMLRequest=fVBND8FAED37F83eqx8kYqIS4kBCNJSD26oNm7SztTMV%2Fr21HLiY05vMe2%2FmzYhkXTUwafmCG3VtFXFwrysk8INMtBbBSNIEKGtFwCVsJ6slpN0YGmvYlKYSgavOzGk1StYGM3FhbgiiSJ8UsuZHbs3NYbuzVbc0tVcsZpl4A6JWLZBYImcijdMkjAdhMizSBHo9iPsHT8s%2F66YaTxrP%2F287vkkE86LIw3y9LUSwV5b8dY4gxqNXQvC77Vfm%2F7aSSNlXRKePvgw%2B3e8nx08%3D&RelayState=",
-      id_me_signup_link:
-        "https://identityProviderUrl.com?SAMLRequest=fVBND8FAED37F83eqx8kYqIS4kBCNJSD26oNm7SztTMV%2Fr21HLiY05vMe2%2FmzYhkXTUwafmCG3VtFXFwrysk8INMtBbBSNIEKGtFwCVsJ6slpN0YGmvYlKYSgavOzGk1StYGM3FhbgiiSJ8UsuZHbs3NYbuzVbc0tVcsZpl4A6JWLZBYImcijdMkjAdhMizSBHo9iPsHT8s%2F66YaTxrP%2F287vkkE86LIw3y9LUSwV5b8dY4gxqNXQvC77Vfm%2F7aSSNlXRKePvgw%2B3e8nx08%3D&RelayState=&op=signup",
-    };
     function render(template, authOptions) {
       return new Promise((resolve, reject) => {
         try {
           expect(template).toBe("login_selection");
           expect(authOptions.id_me_login_link).not.toBeNull();
-          expect(authOptions.id_me_login_link).toContain('https://identityProviderUrl.com?SAMLRequest=');
-          expect(authOptions.id_me_login_link).toContain('&RelayState=');
-          expect(authOptions.dslogon_login_link).toContain('https://identityProviderUrl.com?SAMLRequest=');
-          expect(authOptions.dslogon_login_link).toContain('&RelayState=');
-          expect(authOptions.mhv_login_link).toContain('https://identityProviderUrl.com?SAMLRequest=');
-          expect(authOptions.mhv_login_link).toContain('&RelayState=');
-          expect(authOptions.id_me_signup_link).toContain('https://identityProviderUrl.com?SAMLRequest=');
-          expect(authOptions.id_me_signup_link).toContain('&RelayState=');
-          expect(authOptions.id_me_signup_link).toContain('&op=signup');
-
-          // expect(authOptions.id_me_login_link.toString().includes("FAED37")).toEqual(true);
-          // expect(authOptions.id_me_login_link).toContain('https://identityProviderUrl.com?SAMLRequest=f');
-          // expect(authOptions.id_me_login_link).toBe(
-          //   expectedAuthOpts.id_me_login_link
-          // );
-          // expect(mockNext).not.toHaveBeenCalled();
-          // expect(mock_render).toHaveBeenCalledWith("login_selection", {
-          //   id_me_login_link:
-          //     "https://identityProviderUrl.com?SAMLRequest=fVBND8FAED37F83eqx8kYqIS4kBCNJSD26oNm7SztTMV%2Fr21HLiY05vMe2%2FmzYhkXTUwafmCG3VtFXFwrysk8INMtBbBSNIEKGtFwCVsJ6slpN0YGmvYlKYSgavOzGk1StYGM3FhbgiiSJ8UsuZHbs3NYbuzVbc0tVcsZpl4A6JWLZBYImcijdMkjAdhMizSBHo9iPsHT8s%2F66YaTxrP%2F287vkkE86LIw3y9LUSwV5b8dY4gxqNXQvC77Vfm%2F7aSSNlXRKePvgw%2B3e8nx08%3D&RelayState=",
-          //   dslogon_login_link:
-          //     "https://identityProviderUrl.com?SAMLRequest=fVBND8FAED37F83eqx8kYqIS4kBCNJSD26oNm7SztTMV%2Fr21HLiY05vMe2%2FmzYhkXTUwafmCG3VtFXFwrysk8INMtBbBSNIEKGtFwCVsJ6slpN0YGmvYlKYSgavOzGk1StYGM3FhbgiiSJ8UsuZHbs3NYbuzVbc0tVcsZpl4A6JWLZBYImcijdMkjAdhMizSBHo9iPsHT8s%2F66YaTxrP%2F287vkkE86LIw3y9LUSwV5b8dY4gxqNXQvC77Vfm%2F7aSSNlXRKePvgw%2B3e8nx08%3D&RelayState=",
-          //   mhv_login_link:
-          //     "https://identityProviderUrl.com?SAMLRequest=fVBND8FAED37F83eqx8kYqIS4kBCNJSD26oNm7SztTMV%2Fr21HLiY05vMe2%2FmzYhkXTUwafmCG3VtFXFwrysk8INMtBbBSNIEKGtFwCVsJ6slpN0YGmvYlKYSgavOzGk1StYGM3FhbgiiSJ8UsuZHbs3NYbuzVbc0tVcsZpl4A6JWLZBYImcijdMkjAdhMizSBHo9iPsHT8s%2F66YaTxrP%2F287vkkE86LIw3y9LUSwV5b8dY4gxqNXQvC77Vfm%2F7aSSNlXRKePvgw%2B3e8nx08%3D&RelayState=",
-          //   id_me_signup_link:
-          //     "https://identityProviderUrl.com?SAMLRequest=fVBND8FAED37F83eqx8kYqIS4kBCNJSD26oNm7SztTMV%2Fr21HLiY05vMe2%2FmzYhkXTUwafmCG3VtFXFwrysk8INMtBbBSNIEKGtFwCVsJ6slpN0YGmvYlKYSgavOzGk1StYGM3FhbgiiSJ8UsuZHbs3NYbuzVbc0tVcsZpl4A6JWLZBYImcijdMkjAdhMizSBHo9iPsHT8s%2F66YaTxrP%2F287vkkE86LIw3y9LUSwV5b8dY4gxqNXQvC77Vfm%2F7aSSNlXRKePvgw%2B3e8nx08%3D&RelayState=&op=signup",
-          // });
+          expect(authOptions.id_me_login_link).toContain(
+            "https://identityProviderUrl.com?SAMLRequest="
+          );
+          expect(authOptions.id_me_login_link).toContain("&RelayState=");
+          expect(authOptions.dslogon_login_link).toContain(
+            "https://identityProviderUrl.com?SAMLRequest="
+          );
+          expect(authOptions.dslogon_login_link).toContain("&RelayState=");
+          expect(authOptions.mhv_login_link).toContain(
+            "https://identityProviderUrl.com?SAMLRequest="
+          );
+          expect(authOptions.mhv_login_link).toContain("&RelayState=");
+          expect(authOptions.id_me_signup_link).toContain(
+            "https://identityProviderUrl.com?SAMLRequest="
+          );
+          expect(authOptions.id_me_signup_link).toContain("&RelayState=");
+          expect(authOptions.id_me_signup_link).toContain("&op=signup");
           resolve(true);
         } catch (err) {
           reject(err);
