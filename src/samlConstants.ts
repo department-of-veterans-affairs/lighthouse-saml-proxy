04fbd5c94538b4360cff0ac8f4e351ed240d8f24
Derek Brown <derek.brown@libertyits.com>
2021-11-01 10:39:08 -0400

    Added reference links to constants

--- src/samlConstants.ts
+++ src/samlConstants.ts
@@ -3,12 +3,14 @@ export const BINDINGS = {
   POST: "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
 };
 
+//https://developers.login.gov/saml/#identity-assurance-level-ial
 export const IDENTITYPROTOCOL = {
   BASIC: "http://idmanagement.gov/ns/assurance/ial/1",
   IDENTITY_VERIFIED: "http://idmanagement.gov/ns/assurance/ial/2",
   IDENTITY_VERIFIED_LIVENESS: "http://idmanagement.gov/ns/assurance/aal/3",
 };
 
+//https://developers.login.gov/saml/#authentication-assurance-level-aal
 export const PASSWORDPROTOCOL = {
   DEFAULT: "urn:gov:gsa:ac:classes:sp:PasswordProtectedTransport:duo",
   MULTIFACTOR_TWELVE: "http://idmanagement.gov/ns/assurance/aal/2",
