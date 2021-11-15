515198274b4effe713a73b990e1ae21559103e3f
Larry Crochet <larry.crochet@libertyits.com>
2021-11-15 11:13:08 -0500

    signup op

--- src/SPConfig.js
+++ src/SPConfig.js
@@ -47,6 +47,7 @@ export default class SPConfig {
     this.failureFlash = true;
     this.category = argv.category || "id_me";
     this.signupLinkEnabled = argv.spIdpSignupLinkEnabled;
+    this.signupOp = argv.spIdpSignupOp;
   }
 
   getMetadataParams(req) {
