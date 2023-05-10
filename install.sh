f3b25e2d510d4463195d733b7b45f21cca85cdca
630811 <mekala_dharani@bah.com>
2023-05-10 13:22:03 -0600

    making script changes

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 # Team Pivot!
 
-if npm install 2>&1 | grep -q "WARN"; then
+output=$(npm install --loglevel=error 2>&1)
+
+if npm "$output" | grep -q "WARN"; then
     echo ERROR: npm WARN detected
     exit 1
 fi
+
+echo "No NPM warnings detected."
+exit 0
