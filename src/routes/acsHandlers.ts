dddd110f3de0df8ba78792fdfcba4cf41b66ecd9
John Weisz <47699943+johnmweisz@users.noreply.github.com>
2023-05-24 16:07:07 -0400

    API-25662 adds InResponseTo SAML attribute (#434)
    * add inResponseTo to config
    
    * refactor
    
    * fix naming
    
    * add start-local
    
    * ignore local-config
    
    * defensive InResponseTo session.id fallback
    
    * remove unneeded nullsafe
    
    * add unit test
    
    * remove import
    
    * add function description
    
    * fix typo

--- src/routes/acsHandlers.ts
+++ src/routes/acsHandlers.ts
@@ -287,12 +287,14 @@ export const serializeAssertions = (
   const time = new Date().toISOString();
   if (req.session) {
     authOptions.RelayState = req.options.ssoResponse.state;
+    authOptions.inResponseTo = req.session.authnRequest?.id || req.session.id;
     const logObj = {
       session: req.sessionID,
       stateFromSession: true,
       step: "to Okta",
       time,
       relayState: authOptions.RelayState,
+      inResponseTo: authOptions.inResponseTo,
     };
 
     logger.info(
