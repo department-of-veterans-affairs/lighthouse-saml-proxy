6bb801f89f6d97a9ec4b65b96575b5a373856512
630811 <mekala_dharani@bah.com>
2024-07-02 09:00:08 -0500

    modifying banner files

--- src/banner.js
+++ src/banner.js
@@ -1,22 +1,21 @@
 class Banner {
-    constructor(id, startTime, endTime, message) {
-        this.id = id; //unique identifier for the banner
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.enabled = false;
-        this.message = message;
-        this.priority = 0;
-        this.order = 0;
-    }
+  constructor(id, startTime, endTime, message) {
+    this.id = id; //unique identifier for the banner
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.enabled = false;
+    this.message = message;
+    this.priority = 0;
+    this.order = 0;
+  }
 
-    setPriority(priority) {
-        this.priority = priority;
-    }
+  setPriority(priority) {
+    this.priority = priority;
+  }
 
-    setOrder(order) {
-        this.order = order;
-    }
-
+  setOrder(order) {
+    this.order = order;
+  }
 }
 
 module.exports = Banner;
